bf41725f533e82e92d6737f137f7ccb4e34e009b
makinate <makinatusa@gmail.com>
2015-04-22 14:55:16 -0400

    R programming Assignment 2 - Solution Max Kinateder
    Coursera Rprog-013
    Programming assignment 2
    Author: Max Kinateder

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Coursera R programming Rprog-013
+#Programming Assignment 2
+#04-22-2015
 
-## Write a short comment describing this function
+
+#cache matrix: This function caches a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) { 
+                x <<- y      
+                s <<- NULL
+        }
+        get <- function() x #get the value of x
+        setsolve <- function(solve) s <<- solve #set value of solve
+        getsolve <- function() s #get the value of the mean
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## this function solves a cached matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
+
